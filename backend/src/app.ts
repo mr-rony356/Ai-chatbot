d4cb5c25a56ec0ef08924bfece160f159feb9a02
omorRony356 <orony@relevance.studio>
2024-03-13 01:06:52 +0600

    frontend added

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
-// import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser";
 // import cors from "cors";
 config();
 const app = express();
@@ -10,7 +10,8 @@ const app = express();
 //middlewares
 // app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
-// app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+console.log(process.env.COOKIE_SECRET)
 
 // //remove it in production
 app.use(morgan("dev"));
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user-controllers.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user-controllers.js";
 
 const userRoutes = Router();
 userRoutes.get('/',getAllUsers)
 userRoutes.post('/signup',userSignup)
-userRoutes.post('/login',userSignup)
+userRoutes.post('/login',userLogin)
 
 
 export default userRoutes;
